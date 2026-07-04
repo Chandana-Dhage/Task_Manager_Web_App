df7ea8afff222481b28dc1bc3965677b30e87958
Chandana <chandudhage88@gmail.com>
2026-07-04 14:13:11 +0530

    deployment dependencies issue

--- backend/config.py
+++ backend/config.py
@@ -1,19 +1,24 @@
 import os
-import os
 
 print("HOST:", os.getenv("DB_HOST"))
+print("PORT:", os.getenv("DB_PORT"))
 print("USER:", os.getenv("DB_USER"))
 print("DATABASE:", os.getenv("DB_NAME"))
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT", "3306")
+    DB_NAME = os.getenv("DB_NAME")
+
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{os.getenv('DB_USER')}:"
-        f"{os.getenv('DB_PASSWORD')}@"
-        f"{os.getenv('DB_HOST')}/"
-        f"{os.getenv('DB_NAME')}"
+        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
+        f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+        "?ssl_verify_cert=false"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
